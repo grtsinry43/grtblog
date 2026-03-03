b027776425cbdb8281f34f8d57c14121bc7945b3
grtsinry43 <grtsinry43@outlook.com>
2026-03-03 19:37:33 +0800

    feat: remove unique constraint on user email to allow multiple OAuth users with the same email

--- server/migrations/0042_drop_user_email_unique.sql
+++ server/migrations/0042_drop_user_email_unique.sql
@@ -1,3 +1,20 @@
+-- +goose Up
 -- 去除 app_user 表的邮箱唯一约束。
 -- 邮箱不是主要标识符（主键为 id，用户名唯一），允许多个 OAuth 用户拥有相同邮箱。
 ALTER TABLE app_user DROP CONSTRAINT IF EXISTS uq_app_user_email;
+
+-- +goose Down
+-- +goose StatementBegin
+DO $$
+BEGIN
+    IF NOT EXISTS (
+        SELECT 1
+        FROM pg_constraint
+        WHERE conname = 'uq_app_user_email'
+    ) THEN
+        ALTER TABLE app_user
+            ADD CONSTRAINT uq_app_user_email UNIQUE (email);
+    END IF;
+END
+$$;
+-- +goose StatementEnd
